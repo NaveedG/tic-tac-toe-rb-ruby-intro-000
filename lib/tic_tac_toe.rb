35759f5f368a98acc0502aa74d1b94cce91b1805
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 17:21:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,9 +38,10 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   position = input_to_index(user_input)
+  token == "X" || token == "O"
 
   if valid_move?(board, position)
-    move(board, position, token= "X" || token= "O")
+    move(board, position, token)
     display_board(board)
   else
     turn(board)
