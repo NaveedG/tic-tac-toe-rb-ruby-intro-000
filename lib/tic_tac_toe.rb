1603de97f5078f469ee7cb7ee8d42e21ca969858
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 16:35:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
 
-  if valid_move?(board, index, token)
-    move(board, index)
+  if valid_move?(board, index)
+    move(board, index, token)
     display_board(board)
   else
     turn(board)
