de3a0cd593b21e1ed879016bf9eba17bdde0c4d1
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 16:37:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def turn(board)
   index = input_to_index(user_input)
 
   if valid_move?(board, index)
-    move(board, index, token)
+    move(board, position, token)
     display_board(board)
   else
     turn(board)
