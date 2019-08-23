4e446ea29ae7504a933feb191341e77fe8400828
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 16:56:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,3 +63,23 @@ def current_player(board)
     return "O"
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    win_index_1 = combo[0]
+    win_index_2 = combo[1]
+    win_index_3 = combo[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combo
+    end
+
+  end
+  return false
+end
